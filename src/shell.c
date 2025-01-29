c69ee4e026904ce34b23bd24e0bf616c35881d95
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2025-01-29 18:26:14 +0100

    m

--- src/shell.c
+++ src/shell.c
@@ -264,11 +264,12 @@ static void
 on_proximity_near_changed (PhoshShell *self)
 {
   PhoshShellPrivate *priv;
-  gboolean near = phosh_proximity_near (priv->proximity);
+  gboolean near;
 
   g_warning("on_proximity_near_changed");
   priv = phosh_shell_get_instance_private (self);
 
+  near = phosh_proximity_near (priv->proximity);
   phosh_screen_saver_manager_suspend_autolock (priv->screen_saver_manager, near);
   phosh_shell_enable_power_save (phosh_shell_get_default (), near);
 }
